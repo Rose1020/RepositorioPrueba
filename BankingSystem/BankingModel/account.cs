4bacc12eae24663d70c78baaa65e1a8f0534c7da
Clase <Clase>
2017-03-09 21:42:13 -0500

    clase1

---
+++ BankingSystem/BankingModel/account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingModel
+{
+    public class Account
+    {
+
+        private string _name;
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+        //private string _name;
+        //public string  getName()
+        //{
+        //    return _name;
+ 
+        //}
+        //public void setName(string name)
+        //{
+        //    _name = name;
+ 
+        //}
+         //Manera alternativa
+        public string  Name
+        {
+            get 
+            {
+               return _name;
+            }
+            set
+            {
+                _name = value; 
+            }
+        }
+
+        //public string Name { get; set; }
+
+    }
+
+}
---
+++ BankingSystem/ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using BankingModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            Console.Write("Nombre inicial de la cuenta {0}", myAccount.Name);
+            
+            
+            
+        }
+    }
+}
